ec2f4da019a3c5f34be9d34f5e41a1895d83f3aa
Dan Tello <dan.tello@gmail.com>
2014-08-30 22:00:02 -0400

    Modify browserify task for use with multiple bundles.

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -13,41 +13,71 @@ var gulp         = require('gulp');
 var handleErrors = require('../util/handleErrors');
 var source       = require('vinyl-source-stream');
 
-gulp.task('browserify', function() {
-  var bundler = browserify({
-    // Required watchify args
-    cache: {}, packageCache: {}, fullPaths: true,
-    // Specify the entry point of your app
-    entries: ['./src/javascript/app.coffee'],
-    // Add file extentions to make optional in your requires
-    extensions: ['.coffee', '.hbs'],
-    // Enable source maps!
-    debug: true
-  });
+gulp.task('browserify', function(callback) {
 
-  var bundle = function() {
-    // Log when bundling starts
-    bundleLogger.start();
+  // Each of these objects represent separate bundles we'd like to compile
+  var filesToBundle = [{
+      entries: './src/javascript/app.coffee',
+      dest: './build/',
+      bundleName: 'app.js'
+    }, {
+      entries: './src/javascript/admin.coffee',
+      dest: './build/',
+      bundleName: 'admin.js'
+    }];
 
-    return bundler
-      .bundle()
-      // Report compile errors
-      .on('error', handleErrors)
-      // Use vinyl-source-stream to make the
-      // stream gulp compatible. Specifiy the
-      // desired output filename here.
-      .pipe(source('app.js'))
-      // Specify the output destination
-      .pipe(gulp.dest('./build/'))
-      // Log when bundling completes!
-      .on('end', bundleLogger.end);
+  var bundleQueue = filesToBundle.length;
+
+  var browserifyThis = function(file) {
+    var bundler = browserify({
+      // Required watchify args
+      cache: {}, packageCache: {}, fullPaths: true,
+      // Specify the entry point of your app
+      entries: file.entries,
+      // Add file extentions to make optional in your requires
+      extensions: ['.coffee', '.hbs'],
+      // Enable source maps!
+      debug: true
+    });
+
+    var bundle = function() {
+      // Log when bundling starts
+      bundleLogger.start(file.bundleName);
+
+      return bundler
+        .bundle()
+        // Report compile errors
+        .on('error', handleErrors)
+        // Use vinyl-source-stream to make the
+        // stream gulp compatible. Specifiy the
+        // desired output filename here.
+        .pipe(source(file.bundleName))
+        // Specify the output destination
+        .pipe(gulp.dest(file.dest))
+        .on('end', function() {
+          // Log when bundling completes
+          bundleLogger.end(file.bundleName)
+
+          if(bundleQueue.length) {
+            bundleQueue--;
+            if(bundleQueue === 0) {
+              // Let gulp know the task is complete:
+              // all bundles have been initialized.
+              callback();
+            }
+          }
+        });
+    };
+
+    if(global.isWatching) {
+      // Wrap with watchify and rebundle on changes
+      bundler = watchify(bundler);
+      bundler.on('update', bundle);
+    }
+
+    return bundle();
   };
 
-  if(global.isWatching) {
-    bundler = watchify(bundler);
-    // Rebundle with watchify on changes.
-    bundler.on('update', bundle);
-  }
-
-  return bundle();
+  // Browserify each item in the list
+  filesToBundle.forEach(browserifyThis);
 });
---
+++ src/javascript/admin.coffee
@@ -0,0 +1,3 @@
+# Dummy example for the sake of showing multiple bundles
+$ = require('jquery')
+console.log('Admin?')
